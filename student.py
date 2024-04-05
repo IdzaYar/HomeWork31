5e13d4ee148277fd5972d53a97dd884238805a50
IdzaYar <160059368+IdzaYar@users.noreply.github.com>
2024-04-06 01:22:20 +0300

    Add files via upload

---
+++ student.py
@@ -0,0 +1,12 @@
+from group import Human
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return super().__str__() + f", record book: {self.record_book}"
+
+    def __eq__(self, other):
+        return str(self) == str(other)
